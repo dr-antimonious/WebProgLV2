31822896b68a18d57e115c6cf9c6381588649791
Leo Tumbas <leotumbas@gmail.com>
2024-04-22 09:58:31 +0200

    First commit

--- main.ts
+++ main.ts
@@ -1,13 +1,16 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var readline = require("readline");
-var process = require("process");
+
+import readline = require('readline');
+import process = require('process');
+
 // How much money the user has
-var walletAmount = 0;
+let walletAmount : number = 0;
+
 // User's cart
-var cart = [];
+let cart : {name:string, price:number, amount:number}[] = [];
+
 // Available items
-var items = [
+let items : {name:string, price:number}[] = [
     {
         name: 'apple',
         price: 1.99,
@@ -17,42 +20,48 @@ var items = [
         price: 10,
     },
 ];
+
 // Generic function for retrieving an item "from the shelf"
-function getItem(itemName) {
-    var item;
-    try {
-        var index = items.findIndex(function (item) { return item.name === itemName; });
+function getItem(itemName: string) : {name: string, price:number} | null {
+    let item : {name:string, price:number};
+    try{
+        const index:number = items.findIndex((item : {name:string, price:number}) : boolean => item.name === itemName);
         item = items[index];
     }
-    catch (err) {
+    catch (err){
         console.error(err);
         return null;
     }
     return item;
 }
-function addToCart(itemName, amount) {
-    var item = getItem(itemName);
+
+function addToCart(itemName:string, amount:number) : boolean {
+    const item : {name:string, price:number} | null = getItem(itemName);
     if (item == null) {
         console.log("Unknown item.");
         return false;
     }
-    var cartIndex;
-    if ((cartIndex = cart.findIndex(function (item) { return item.name === itemName; })) !== -1) {
+
+    let cartIndex : number;
+    if ((cartIndex = cart.findIndex((item : {name:string, price:number}) : boolean => item.name === itemName)) !== -1) {
         cart[cartIndex].amount += amount;
     }
     else {
-        cart.push({ name: item.name, price: item.price, amount: amount });
+        cart.push({name: item.name, price: item.price, amount: amount});
     }
+
     return true;
 }
-function removeFromCart(itemName, amount) {
-    var item = getItem(itemName);
+
+function removeFromCart(itemName:string, amount:number) : boolean {
+    const item : {name:string, price:number} | null = getItem(itemName);
     if (item == null) {
         console.log("Unknown item.");
         return false;
     }
-    var cartIndex;
-    if ((cartIndex = cart.findIndex(function (item) { return item.name === itemName; })) !== -1) {
+
+    let cartIndex : number;
+    if ((cartIndex = cart.findIndex((item : {name:string, price:number}) : boolean => item.name === itemName)) !== -1) {
         if (cart[cartIndex].amount < amount) {
             console.log("Invalid amount to be removed.");
             return false;
@@ -70,39 +79,53 @@ function removeFromCart(itemName, amount) {
         return false;
     }
 }
-function viewCart() {
-    cart.forEach(function (item) { return console.log(item); });
+
+function viewCart() : void {
+    cart.forEach(item => console.log(item));
 }
-function topUp(amount) {
+
+function clearCart() : void {
+    cart.forEach(_ => cart.pop());
+}
+
+// Increase balance
+function topUp(amount: number) : void {
     walletAmount += amount;
     console.log("New balance: ", walletAmount);
 }
-function checkBalance() {
+
+function checkBalance() : void {
     console.log(walletAmount);
 }
-function buy(itemName) {
-    var item = getItem(itemName);
+
+function buy(itemName: string) : boolean {
+    const item:{name:string, price:number} | null = getItem(itemName);
     if (item == null) {
         console.log("Unknown item.");
         return false;
     }
-    console.log("Buying ".concat(item.name, " with price ").concat(item.price));
+    console.log(`Buying ${item.name} with price ${item.price}`);
+
     if (item.price > walletAmount) {
         console.log('Not enough money');
         return false;
     }
+
     return true;
 }
-var rl = readline.createInterface({
+
+const rl:readline.Interface = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
 });
+
 console.log("Store");
 rl.prompt();
-rl.on('line', function (line) {
-    var split = line.toLowerCase().split(' ');
-    var command = split[0];
-    var args = split.slice(1);
+rl.on('line', (line:string):void => {
+    const split:string[] = line.toLowerCase().split(' ');
+    const command:string = split[0];
+    const args:string[] = split.slice(1);
+
     switch (command) {
         case 'add':
             addToCart(args[0], Number(args[1]));
@@ -116,6 +139,9 @@ rl.on('line', function (line) {
         case 'cart':
             viewCart();
             break;
+        case 'clear':
+            clearCart();
+            break;
         case 'buy':
             buy(args[0]);
             break;
@@ -123,11 +149,12 @@ rl.on('line', function (line) {
             removeFromCart(args[0], Number(args[1]));
             break;
         default:
-            console.log("Unknown action: '".concat(command, "'."));
-            console.log("Use 'help' for a list of all acceptable actions.");
+            console.log(`Unknown action: '${command}'.`);
+            console.log("Use 'help' for a list of all acceptable actions.")
     }
+
     rl.prompt();
-}).on('close', function () {
+}).on('close', ():void => {
     console.log('Exit');
     process.exit(0);
 });
